76fa4cf7a6b36f968f9cf1c7b4f7f1c83d3446da
Aaron Kling <webgeek1234@gmail.com>
2021-12-10 00:56:20 -0600

    Update twrp support for Android 11
    Change-Id: I5dde9babc4e069452fba0d8462b5e34cbb4d422b

--- twrp_mdarcy.mk
+++ twrp_mdarcy.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,12 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_porg.mk \
-    $(LOCAL_DIR)/lineage_porg.mk \
-    $(LOCAL_DIR)/twrp_porg.mk
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit device configuration for porg.
+include device/nvidia/foster/lineage.mk
+$(call inherit-product, device/nvidia/porg/full_porg.mk)
+
+PRODUCT_NAME := twrp_porg
+PRODUCT_DEVICE := porg
