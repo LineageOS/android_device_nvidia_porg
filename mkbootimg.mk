1794617f0013165ec09b783846ab58b8ba0fbbba
Aaron Kling <webgeek1234@gmail.com>
2026-08-26 13:54:27 -0500

    mkbootimg: Copy partition xmls to out
    For some reason, tegraflash opens these for writing, even though never
    does so for this use case. And aosp sandboxing now blocks this. Fix by
    copying them to out and have tegraflash access those.
    
    Change-Id: I02f5eb7a50213bae6ce754070b555a36559b57df

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -41,36 +41,37 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOOTIM
 	$(hide) $(MKBOOTIMG) --kernel $(call bootimage-to-kernel,$@) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $(dir $@)/boot.tmp
 	$(hide )$(call assert-max-image-size,$(dir $@)/boot.tmp,$(call get-bootimage-partition-size,$@,boot))
 	$(hide) cp $(FOSTER_BCT)/P3448_A00_lpddr4_204Mhz_P987.cfg $(dir $@)/P3448_A00_lpddr4_204Mhz_P987.cfg
-	$(hide) cd $(dir $@); PYTHONDONTWRITEBYTECODE=1 $(TEGRAFLASH_PATH)/tegraflash.py --chip 0x21 --applet $(T210_PATH)/nvtboot_recovery.bin --bct P3448_A00_lpddr4_204Mhz_P987.cfg --cfg $(abspath $(BCT_PATH))/signboot.xml --cmd "sign"
+	$(hide) cp $(BCT_PATH)/*.xml $(dir $@)/
+	$(hide) cd $(dir $@); PYTHONDONTWRITEBYTECODE=1 $(TEGRAFLASH_PATH)/tegraflash.py --chip 0x21 --applet $(T210_PATH)/nvtboot_recovery.bin --bct P3448_A00_lpddr4_204Mhz_P987.cfg --cfg signboot.xml --cmd "sign"
 	$(hide) mv $(dir $@)/signed $(dir $@)/signed_boot
 	@mkdir -p $(PRODUCT_OUT)/install
 	$(hide) cp $(JETSON_BL)/porg/tegra210-p3448-0002-p3449-0000-a02.dtb $(dir $@)/
 	$(hide) cp $(DTB_PATH)/tegra210-p3448-0002-p3449-0000-a02-android-devkit.dtb $(dir $@)/
-	$(hide) cd $(dir $@); PYTHONDONTWRITEBYTECODE=1 $(TEGRAFLASH_PATH)/tegraflash.py --chip 0x21 --applet $(T210_PATH)/nvtboot_recovery.bin --bct P3448_A00_lpddr4_204Mhz_P987.cfg --cfg $(abspath $(BCT_PATH))/signporga02.xml --cmd "sign"
+	$(hide) cd $(dir $@); PYTHONDONTWRITEBYTECODE=1 $(TEGRAFLASH_PATH)/tegraflash.py --chip 0x21 --applet $(T210_PATH)/nvtboot_recovery.bin --bct P3448_A00_lpddr4_204Mhz_P987.cfg --cfg signporga02.xml --cmd "sign"
 	$(hide) mv $(dir $@)/signed $(dir $@)/signed_porga02
 	$(hide) cp $(dir $@)/signed_porga02/tegra210-p3448-0002-p3449-0000-a02.dtb.encrypt $(dir $@)/install/tegra210-p3448-0002-p3449-0000-a02.dtb
 	$(hide) cp $(dir $@)/signed_porga02/tegra210-p3448-0002-p3449-0000-a02-android-devkit.dtb.encrypt $(dir $@)/install/tegra210-p3448-0002-p3449-0000-a02-android-devkit.dtb
 	$(hide) cp $(JETSON_BL)/porg/tegra210-p3448-0002-p3449-0000-b00.dtb $(dir $@)/
 	$(hide) cp $(DTB_PATH)/tegra210-p3448-0002-p3449-0000-b00-android-devkit.dtb $(dir $@)/
-	$(hide) cd $(dir $@); PYTHONDONTWRITEBYTECODE=1 $(TEGRAFLASH_PATH)/tegraflash.py --chip 0x21 --applet $(T210_PATH)/nvtboot_recovery.bin --bct P3448_A00_lpddr4_204Mhz_P987.cfg --cfg $(abspath $(BCT_PATH))/signporgb00.xml --cmd "sign"
+	$(hide) cd $(dir $@); PYTHONDONTWRITEBYTECODE=1 $(TEGRAFLASH_PATH)/tegraflash.py --chip 0x21 --applet $(T210_PATH)/nvtboot_recovery.bin --bct P3448_A00_lpddr4_204Mhz_P987.cfg --cfg signporgb00.xml --cmd "sign"
 	$(hide) mv $(dir $@)/signed $(dir $@)/signed_porgb00
 	$(hide) cp $(dir $@)/signed_porgb00/tegra210-p3448-0002-p3449-0000-b00.dtb.encrypt $(dir $@)/install/tegra210-p3448-0002-p3449-0000-b00.dtb
 	$(hide) cp $(dir $@)/signed_porgb00/tegra210-p3448-0002-p3449-0000-b00-android-devkit.dtb.encrypt $(dir $@)/install/tegra210-p3448-0002-p3449-0000-b00-android-devkit.dtb
 	$(hide) cp $(JETSON_BL)/porg/tegra210-p3448-0000-p3449-0000-a02.dtb $(dir $@)/
 	$(hide) cp $(DTB_PATH)/tegra210-p3448-0000-p3449-0000-a02-android-devkit.dtb $(dir $@)/
-	$(hide) cd $(dir $@); PYTHONDONTWRITEBYTECODE=1 $(TEGRAFLASH_PATH)/tegraflash.py --chip 0x21 --applet $(T210_PATH)/nvtboot_recovery.bin --bct P3448_A00_lpddr4_204Mhz_P987.cfg --cfg $(abspath $(BCT_PATH))/signporgsda02.xml --cmd "sign"
+	$(hide) cd $(dir $@); PYTHONDONTWRITEBYTECODE=1 $(TEGRAFLASH_PATH)/tegraflash.py --chip 0x21 --applet $(T210_PATH)/nvtboot_recovery.bin --bct P3448_A00_lpddr4_204Mhz_P987.cfg --cfg signporgsda02.xml --cmd "sign"
 	$(hide) mv $(dir $@)/signed $(dir $@)/signed_porgsda02
 	$(hide) cp $(dir $@)/signed_porgsda02/tegra210-p3448-0000-p3449-0000-a02.dtb.encrypt $(dir $@)/install/tegra210-p3448-0000-p3449-0000-a02.dtb
 	$(hide) cp $(dir $@)/signed_porgsda02/tegra210-p3448-0000-p3449-0000-a02-android-devkit.dtb.encrypt $(dir $@)/install/tegra210-p3448-0000-p3449-0000-a02-android-devkit.dtb
 	$(hide) cp $(JETSON_BL)/porg/tegra210-p3448-0000-p3449-0000-b00.dtb $(dir $@)/
 	$(hide) cp $(DTB_PATH)/tegra210-p3448-0000-p3449-0000-b00-android-devkit.dtb $(dir $@)/
-	$(hide) cd $(dir $@); PYTHONDONTWRITEBYTECODE=1 $(TEGRAFLASH_PATH)/tegraflash.py --chip 0x21 --applet $(T210_PATH)/nvtboot_recovery.bin --bct P3448_A00_lpddr4_204Mhz_P987.cfg --cfg $(abspath $(BCT_PATH))/signporgsdb00.xml --cmd "sign"
+	$(hide) cd $(dir $@); PYTHONDONTWRITEBYTECODE=1 $(TEGRAFLASH_PATH)/tegraflash.py --chip 0x21 --applet $(T210_PATH)/nvtboot_recovery.bin --bct P3448_A00_lpddr4_204Mhz_P987.cfg --cfg signporgsdb00.xml --cmd "sign"
 	$(hide) mv $(dir $@)/signed $(dir $@)/signed_porgsdb00
 	$(hide) cp $(dir $@)/signed_porgsdb00/tegra210-p3448-0000-p3449-0000-b00.dtb.encrypt $(dir $@)/install/tegra210-p3448-0000-p3449-0000-b00.dtb
 	$(hide) cp $(dir $@)/signed_porgsdb00/tegra210-p3448-0000-p3449-0000-b00-android-devkit.dtb.encrypt $(dir $@)/install/tegra210-p3448-0000-p3449-0000-b00-android-devkit.dtb
 	$(hide) cp $(JETSON_BL)/porg/tegra210-p3448-0003-p3542-0000.dtb $(dir $@)/
 	$(hide) cp $(DTB_PATH)/tegra210-p3448-0003-p3542-0000-android-devkit.dtb $(dir $@)/
-	$(hide) cd $(dir $@); PYTHONDONTWRITEBYTECODE=1 $(TEGRAFLASH_PATH)/tegraflash.py --chip 0x21 --applet $(T210_PATH)/nvtboot_recovery.bin --bct P3448_A00_lpddr4_204Mhz_P987.cfg --cfg $(abspath $(BCT_PATH))/signbatuu.xml --cmd "sign"
+	$(hide) cd $(dir $@); PYTHONDONTWRITEBYTECODE=1 $(TEGRAFLASH_PATH)/tegraflash.py --chip 0x21 --applet $(T210_PATH)/nvtboot_recovery.bin --bct P3448_A00_lpddr4_204Mhz_P987.cfg --cfg signbatuu.xml --cmd "sign"
 	$(hide) mv $(dir $@)/signed $(dir $@)/signed_batuu
 	$(hide) cp $(dir $@)/signed_batuu/tegra210-p3448-0003-p3542-0000.dtb.encrypt $(dir $@)/install/tegra210-p3448-0003-p3542-0000.dtb
 	$(hide) cp $(dir $@)/signed_batuu/tegra210-p3448-0003-p3542-0000-android-devkit.dtb.encrypt $(dir $@)/install/tegra210-p3448-0003-p3542-0000-android-devkit.dtb
@@ -79,6 +80,6 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOOTIM
 # Depend on boot.img to prevent tegraflash signing from running simultaneously
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(recoveryimage-deps) $(RECOVERYIMAGE_EXTRA_DEPS) $(INSTALLED_BOOTIMAGE_TARGET)
 	$(call build-recoveryimage-target, $(dir $@)/recovery.tmp, $(recovery_kernel))
-	$(hide) cd $(dir $@); PYTHONDONTWRITEBYTECODE=1 $(TEGRAFLASH_PATH)/tegraflash.py --chip 0x21 --applet $(T210_PATH)/nvtboot_recovery.bin --bct P3448_A00_lpddr4_204Mhz_P987.cfg --cfg $(abspath $(BCT_PATH))/signrecovery.xml --cmd "sign"
+	$(hide) cd $(dir $@); PYTHONDONTWRITEBYTECODE=1 $(TEGRAFLASH_PATH)/tegraflash.py --chip 0x21 --applet $(T210_PATH)/nvtboot_recovery.bin --bct P3448_A00_lpddr4_204Mhz_P987.cfg --cfg signrecovery.xml --cmd "sign"
 	$(hide) mv $(dir $@)/signed $(dir $@)/signed_recovery
 	$(hide) cp $(dir $@)/signed_recovery/recovery.tmp.encrypt $@
